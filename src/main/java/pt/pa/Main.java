4368debc92b2402dfcf88152baaefb8abdf4ea63
Bruno Russo <russo.bruno99@gmail.com>
2023-11-06 11:08:28 +0000

    Update Main.java

--- src/main/java/pt/pa/Main.java
+++ src/main/java/pt/pa/Main.java
@@ -5,6 +5,7 @@ import pt.pa.controller.FileManager;
 import pt.pa.model.Element;
 import pt.pa.model.File;
 import pt.pa.model.Folder;
+import pt.pa.view.FileManagerApplication;
 
 /**
  *
@@ -16,24 +17,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        /*Folder rootFolder = new Folder();
-        TreeLinked<Element> treeElements = new TreeLinked<>(rootFolder);
-        Folder escola = new Folder(rootFolder, "Escola");
-        Folder pessoal = new Folder(escola, "Pessoal");
-        treeElements.insert(treeElements.getPosition(rootFolder), escola);
-        treeElements.insert(treeElements.getPosition(rootFolder), pessoal);
-
-        File teste = new File(escola, "Teste do Bruno", "Muito mau");
-
-        treeElements.insert(treeElements.getPosition(escola), teste);
-        System.out.println(treeElements.toString());*/
-
-        FileManager fileManager = new FileManager();
-        fileManager.createRootFolder();
-
-
+        FileManagerApplication.main(args);
     }
-
-
 }
