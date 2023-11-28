ecf9d5206924ecad290c4a0264410b70f9329f5d
Rafael Martins <rafaemartins@deloitte.pt>
2023-11-28 13:53:57 +0000

    Update FileManagerTest.java

--- src/test/java/pt/pa/model/FileManagerTest.java
+++ src/test/java/pt/pa/model/FileManagerTest.java
@@ -9,28 +9,43 @@ import pt.pa.model.Folder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * A classe FileManagerTest contém testes unitários para validar o comportamento da classe FileManager.
+ */
 public class FileManagerTest {
     FileManager fileManager;
     Folder rootFolder;
 
+    /**
+     * Configuração inicial para os testes, criando uma instância de FileManager e a pasta raiz (rootFolder).
+     */
     @BeforeEach
     void setUp(){
         this.fileManager = new FileManager();
         rootFolder = fileManager.createRootFolder();
     }
 
+    /**
+     * Testa se a pasta raiz é obtida através do método getRoot().
+     */
     @Test
     @DisplayName("Check get root folder")
     void checkGetRootFolder(){
         assertTrue(fileManager.existsElement(rootFolder));
     }
 
+    /**
+     * Testa se a criação da pasta raiz foi efetuada com sucesso, comparando o nome da pasta raiz com o obtido através do método getRoot().
+     */
     @Test
     @DisplayName("Check create root folder")
     void checkCreateRootElement(){
         assertEquals(rootFolder.getName(), fileManager.getRoot().getName());
     }
 
+    /**
+     * Testa a criação de uma pasta e verifica se a mesma existe após a criação.
+     */
     @Test
     @DisplayName("Check create folder")
     void checkCreateFolder(){
@@ -38,6 +53,9 @@ public class FileManagerTest {
         assertTrue(fileManager.existsElement(testFolder));
     }
 
+    /**
+     * Testa a criação de um ficheiro e verifica se o mesmo existe após a criação.
+     */
     @Test
     @DisplayName("Check create file")
     void checkCreateFile(){
@@ -45,9 +63,11 @@ public class FileManagerTest {
         assertTrue(fileManager.existsElement(testFile));
     }
 
+    /**
+     * Testa o movimento de um elemento para uma nova pasta e verifica se o movimento foi realizado corretamente.
+     */
     @Test
     @DisplayName("Check move element")
-    @Description("Create parent folder and file, both with root folder as parent. After creation, move the file to folder parentFolder.")
     void checkMoveElement(){
         Folder parentFolder = fileManager.createFolder(rootFolder, new Folder(rootFolder, "Parent Folder"));
         File file = fileManager.createFile(rootFolder, new File(rootFolder, "Teste", "Content"));
@@ -55,6 +75,9 @@ public class FileManagerTest {
         assertEquals(fileManager.getTreeElements().parent(fileManager.getTreeElements().getPosition(file)).element().getName(), parentFolder.getName());
     }
 
+    /**
+     * Testa a substituição de um elemento por outro e verifica se a substituição foi realizada corretamente.
+     */
     @Test
     @DisplayName("Check replace element")
     void checkReplaceElement(){
@@ -65,6 +88,9 @@ public class FileManagerTest {
         assertTrue(fileManager.existsElement(replaceFile));
     }
 
+    /**
+     * Testa a remoção de um elemento e verifica se ele não existe após a remoção.
+     */
     @Test
     @DisplayName("Check remove element")
     void checkRemoveElement(){
